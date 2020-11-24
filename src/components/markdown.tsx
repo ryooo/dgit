58c40c739dd58404fa2742fe8f765fa553d10669
ryooo <ryooo.321@gmail.com>
2020-11-25 01:19:44 +0900

    Markdown Options

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { useMarkdownApi } from '~/utils/api'
 import '@styles/components/codemirror.css'
 import 'vickymd/theme/editor_themes/light.css'
@@ -14,31 +14,42 @@ type Props = {
   theme?: string;
 }
 
-const Markdown: React.FC<Props> = ({theme}) => {
+const Markdown: React.FC<Props> = (props) => {
+  const [editor, setEditor] = useState(null)
+  const [theme, setTheme] = useState(props.theme)
   const [markdown, setMarkdown] = useState(null)
+  const textareaEl = useRef(null)
   const [options, setOptions] = useState({
     mode: "hypermd",
     lineNumbers: false,
     indentUnit: 2,
-    theme: theme,
   })
   const { data, error } = useMarkdownApi('sample_docs/test.md');
 
   useEffect(() => {
-    const textarea: any = document.getElementById("editor")
-    if (textarea != null) {
-      let editor = VickyMD.fromTextArea(textarea);
+    if (textareaEl != null) {
+      let editor = VickyMD.fromTextArea(textareaEl.current);
       editor.setValue(data.markdown);
       editor.setSize("100%", "100%");
+      setEditor(editor);
     }
-  }, []);
+  }, [textareaEl]);
+
+  useEffect(() => {
+    if (editor !== null) {
+      for (const key in options) {
+          editor.setOption(key as any, options[key]);
+      }
+      editor.setOption("theme", props.theme);
+    }
+  }, [options, editor, props.theme]);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
   return (
       <>
-        <textarea id="editor" style={{display: "none"}}></textarea>
+        <textarea id="editor" style={{display: "none"}} ref={textareaEl}></textarea>
       </>
     );
 }
